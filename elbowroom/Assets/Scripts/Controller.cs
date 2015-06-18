7099d19070902c62ee2870ff3659f1dc6066e34c
Plenopterix <gabrielajzen@gmail.com>
2015-06-18 17:09:58 +0200

    Added Basic Controls To The Cube

---
+++ elbowroom/Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	Vector3 forward;
+
+	void Start () {
+		forward = new Vector3 (0, 0, 20);
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			GetComponent<Rigidbody>().AddForce(forward);
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			GetComponent<Rigidbody>().AddForce(-forward);
+		}
+	}
+}
