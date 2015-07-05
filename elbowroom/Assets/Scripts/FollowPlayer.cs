6dc571bd1276190c6bcdfd1b823018be9671d3d5
Plenopterix <gabrielajzen@gmail.com>
2015-07-05 20:05:45 +0200

    Created a very basic version of the game

---
+++ elbowroom/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject playerToFollow;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		transform.position = new Vector3(transform.position.x, transform.position.y, playerToFollow.transform.position.z - 8);
+	}
+}
