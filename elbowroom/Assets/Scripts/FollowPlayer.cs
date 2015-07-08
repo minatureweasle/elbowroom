d62f6c09d38822b0cefb1f18f3ddb4a738043c7e
minatureweasle <jg123jg123@gmail.com>
2015-07-08 20:17:37 +0100

    Created a Room in the Test Room Scene
    Did it to see what movement will feel like in an actual room.

--- elbowroom/Assets/Scripts/FollowPlayer.cs
+++ elbowroom/Assets/Scripts/FollowPlayer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
 
 	public GameObject playerToFollow;
+	public int panSpeed = 2;
 
 	void Start () {
 	
@@ -11,6 +12,23 @@ public class FollowPlayer : MonoBehaviour {
 
 	void Update () {
 
+
 		transform.position = new Vector3(transform.position.x, transform.position.y, playerToFollow.transform.position.z - 8);
+
+		if (Mathf.Abs (playerToFollow.transform.position.x - transform.position.x) > 5) {
+			Pan ();
+		} else if (Mathf.Abs (playerToFollow.transform.position.x - transform.position.x) < 1) {
+			GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x /3,0,GetComponent<Rigidbody>().velocity.z);
+		}
+
+	}
+
+	void Pan(){
+			if (transform.position.x < playerToFollow.transform.position.x){
+				GetComponent<Rigidbody>().velocity += new Vector3(1,0,0);
+			}
+			else if (transform.position.x > playerToFollow.transform.position.x){
+				GetComponent<Rigidbody>().velocity -= new Vector3(1,0,0);
+			}
 	}
 }
