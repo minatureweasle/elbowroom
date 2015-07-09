d14d6c626b75ccc27de11b100de9a95733818a13
Plenopterix <gabrielajzen@gmail.com>
2015-07-09 22:25:02 +0200

    Added basic shooting to targeting script

--- elbowroom/Assets/Scripts/TargetPlayer.cs
+++ elbowroom/Assets/Scripts/TargetPlayer.cs
@@ -8,11 +8,18 @@ public class TargetPlayer : MonoBehaviour {
 
 	public float lerpSpeed = 1f;
 
+	float waitEndTime = Mathf.Infinity;
+
+	bool waiting = false;
+
 	void Start () {
 
 	}
 
 	void Update () {
+
+		if (Time.time > waitEndTime)
+			Shoot();
 
 		//from turret (this) to player
 		Vector3 directionToPlayer = player.transform.position - transform.position;
@@ -30,9 +37,6 @@ public class TargetPlayer : MonoBehaviour {
 		//slowing down follow
 		transform.up = Vector3.Lerp(transform.up, directionToPlayer, 0.001f*lerpSpeed);
 
-
-		float distanceToPlayer = directionToPlayer.magnitude;
-
 		//constant speed follow
 		/*
 		Vector3 upDirectionWithEqualDistanceAsDistanceToPlayer = transform.up*distanceToPlayer;
@@ -45,19 +49,47 @@ public class TargetPlayer : MonoBehaviour {
 		transform.up += difference;
 		*/
 
-
+		float distanceToPlayer = directionToPlayer.magnitude;
 		
-		//cast a ray in the direction the turret is facing
+		//a raycast's info can be stored in here later
 		RaycastHit hit;
 
 		//draw a line from this point
 		GetComponent<LineRenderer>().SetPosition(0, transform.position);
 		//to this point
+		//cast a ray in the direction the turret is facing, and check if you hit the player
 		if (Physics.Raycast(transform.position + transform.up*2, transform.up, out hit) && hit.collider.tag == "Player")
+		{
 			GetComponent<LineRenderer>().SetPosition(1, transform.position + transform.up*distanceToPlayer);
+
+			//shoot after waiting this long
+			if (!waiting)
+				Wait(1f);
+		}
 		else
+		{
 			GetComponent<LineRenderer>().SetPosition(1, transform.position + transform.up*100f);
 
+			//no longer targeting the player, so stop getting ready to shoot
+			CancelWait();
+		}
 
+
+	}
+
+	void CancelWait(){
+		waiting = false;
+
+		waitEndTime = Mathf.Infinity;
+	}
+
+	void Wait(float duration){
+		waiting = true;
+
+		waitEndTime = Time.time + duration;
+	}
+
+	void Shoot(){
+		Debug.Log("pew pew pew");
 	}
 }
