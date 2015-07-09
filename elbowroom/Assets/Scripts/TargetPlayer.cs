c941c70a7a3e3ac000921b27be0be7750a04b171
Plenopterix <gabrielajzen@gmail.com>
2015-07-09 20:58:33 +0200

    Added Turrets

---
+++ elbowroom/Assets/Scripts/TargetPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPlayer : MonoBehaviour {
+
+	public GameObject player;
+
+	Vector3 originalUp;
+
+	void Start () {
+		originalUp = transform.up;
+	}
+
+	void Update () {
+
+		//from turret (this) to player
+		Vector3 directionToPlayer = player.transform.position - transform.position;
+
+		//from turret to player
+		Debug.DrawRay(transform.position, directionToPlayer, Color.red);
+
+		//currently facing
+		Debug.DrawRay(transform.position, transform.up, Color.cyan);
+
+
+		//perfect follow
+		//transform.up = directionToPlayer;
+
+		//delayed follow
+		transform.up = Vector3.Lerp(transform.up, directionToPlayer, 0.001f);
+
+		Ray ray = new Ray(transform.position, transform.up);
+
+
+		GetComponent<LineRenderer>().SetPosition(0, transform.position);
+
+		GetComponent<LineRenderer>().SetPosition(1, transform.position + transform.up*100f);
+
+
+	}
+}
