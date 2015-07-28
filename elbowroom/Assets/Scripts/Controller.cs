42018cd1f6b3340de5a006a8a7e517cdae0e51da
Plenopterix <gabrielajzen@gmail.com>
2015-07-28 19:19:27 +0200

    Added a room to test the animations and movement

--- elbowroom/Assets/Scripts/Controller.cs
+++ elbowroom/Assets/Scripts/Controller.cs
@@ -5,18 +5,28 @@ public class Controller : MonoBehaviour {
 
 	Vector3 forward;
 
+	Animator myAnimator;
+
 	void Start () {
 		forward = new Vector3 (0, 0, 20);
+
+		myAnimator = GetComponent<Animator>();
 	}
 
 	void Update () {
 		if (Input.GetKey (KeyCode.W)) 
 		{
 			GetComponent<Rigidbody>().AddForce(forward);
+			myAnimator.SetBool("Walking", true);
+		}
+		else
+		{
+			myAnimator.SetBool("Walking", false);
 		}
 		if (Input.GetKey (KeyCode.S)) 
 		{
 			GetComponent<Rigidbody>().AddForce(-forward);
+
 		}
 	}
 }
