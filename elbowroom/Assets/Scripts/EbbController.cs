831741aec966a3d1175c33c459bbf997b2c5eda1
GalacticHero <gabrielajzen@gmail.com>
2015-08-03 15:16:36 +0200

    Added jumping

--- elbowroom/Assets/Scripts/EbbController.cs
+++ elbowroom/Assets/Scripts/EbbController.cs
@@ -10,13 +10,21 @@ public class EbbController : MonoBehaviour {
 
 	Vector3 wantedVelocity;
 
-	public float strafeSpeed = 10;
+	//public float strafeSpeed = 10;
+
+	public bool faceWhereGoing = true;
+
+	public float gravityMultiplier = 4f;
+
+	public float jumpVelocity = 15f;
 
 	void Start () {
 
 		myAnimator = GetComponent<Animator> ();
 
 		myRigidbody = GetComponent<Rigidbody> ();
+
+		Physics.gravity = Physics.gravity * gravityMultiplier;
 	}
 
 	void Update () {
@@ -24,20 +32,23 @@ public class EbbController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.W)) 
 		{
 			//GetComponent<Rigidbody>().AddForce(transform.forward*20);
-			AccelerateFromToZ(3, 9, 20f*Time.deltaTime);
+			AccelerateFromToZ(3, 18, 10f*Time.deltaTime);
 			myAnimator.SetBool("Walking", true);
+			myAnimator.SetFloat("Speed", wantedVelocity.z);
 		}
 		else if (Input.GetKey (KeyCode.S)) 
 		{
 			//GetComponent<Rigidbody>().AddForce(-transform.forward*20);
-			AccelerateFromToZ(-3, -9, -20f*Time.deltaTime);
+			AccelerateFromToZ(-3, -18, -20f*Time.deltaTime);
 			myAnimator.SetBool("Walking", true);
+			myAnimator.SetFloat("Speed", wantedVelocity.z);
 		}
 		else
 		{
 			myAnimator.SetBool("Walking", false);
 
 			wantedVelocity.z = 0;
+			myAnimator.SetFloat("Speed", 0);
 
 			Vector3 newVelocity = GetComponent<Rigidbody>().velocity;
 			newVelocity.z = 0;
@@ -74,6 +85,35 @@ public class EbbController : MonoBehaviour {
 			newVelocity.x = 0;
 			GetComponent<Rigidbody>().velocity = newVelocity;
 		}
+
+		if (Input.GetKeyDown (KeyCode.Space) &! myAnimator.GetBool("Jumping"))
+			Jump ();
+
+		if (faceWhereGoing)
+			transform.forward = wantedVelocity;
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+
+		if (collision.transform.name == "Floor") {
+
+			myAnimator.SetBool("Jumping", false);
+
+		}
+
+	}
+
+	void Jump(){
+
+		Vector3 newVelocity = GetComponent<Rigidbody>().velocity;
+		newVelocity.y = jumpVelocity;
+		GetComponent<Rigidbody>().velocity = newVelocity;
+
+		myAnimator.SetBool("Jumping", true);
+
+
+
 
 	}
 
