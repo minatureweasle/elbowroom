32a72c57c37e125c4c5d00e4c961a10887515054
minatureweasle <jg123jg123@gmail.com>
2015-08-17 13:11:20 +0200

    Created Player Logic Scrip

---
+++ elbowroom/Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStates : MonoBehaviour {
+
+	public Transform respawnPosition;
+	public enum playerState {ALIVE, DEAD};
+	playerState state;
+
+	void Start () {
+		state = playerState.ALIVE;
+	}
+	
+	void Update () {
+	
+		if (state = playerState.DEAD) {
+			Respawn();
+		}
+
+	}
+
+	void Respawn(){
+		transform.position = respawnPosition;
+	}
+
+	public void setState(playerState s){
+		state = s;
+	}
+}
